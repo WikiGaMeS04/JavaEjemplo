10af87ba5318be28893d9990b4306c7bcfaaf2aa
Tiziano26 <132932851+Tiziano26@users.noreply.github.com>
2024-03-26 22:49:01 -0300

    metodo constructor

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -1,9 +1,13 @@
+package com.mycompany.mavenproject1;
+
+import com.mycompany.mavenproject1.Persona;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.mycompany.mavenproject1;
+
 
 /**
  *
@@ -12,6 +16,12 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Persona p1 = new Persona("Joaquin","andreis");
+        p1.setEdad(18);
+        
+        System.out.println("Mi nombre es"+ p1.getNombre());
+    
+    
+    
     }
 }
